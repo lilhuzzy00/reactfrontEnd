08fa474161dc6a1f2efd4d85712b9e210902a4a0
Godspower <chimabest70@gmail.com>
2021-08-26 14:12:58 +0100

    adding helmet

--- src/Routes.js
+++ src/Routes.js
@@ -25,13 +25,13 @@ import { helmet } from "react-helmet"
 const Routes =()=>{
 	return(
 		<BrowserRouter>
-			<Switch>
-				<Helmet>
+			<Helmet>
 					<meta charSet="utf-8" />
 					<title>My Title</title>
 					<link rel="canonical" href="https://www.houseofgoodiz.com/" />
 					<meta property="og:image:secure_url" content="https://www.houseofgoodiz.com/images/image.png"/>
 				</Helmet>
+			<Switch>
 				<Route path="/" exact component={Home} />
 				<Route path="/shop" exact component={Shop} />
 				<Route path="/cart" exact component={Cart} />
