8d3c38835183e0267f6424fb952d368ab19f7c9c
Godspower <chimabest70@gmail.com>
2021-08-27 21:25:35 +0100

    adding helmet

--- src/core/Product.js
+++ src/core/Product.js
@@ -49,9 +49,9 @@ const Products = (props) => {
 					<title>{product.name}</title>
 					<meta property="og:url" content={`https://www.houseofgoodiz.com/product/${product._id}`}/>
 					<meta property="og:image" content={`${API}/product/photo/${product._id}`}/>
-					<meta property="og:image:type" content="image/jpg" />
-					<meta property="og:image:type" content="image/png" />
-					<meta property="fb:app_id" content={`${FB_APP_ID}`} />
+					{/* <meta property="og:image:type" content="image/jpg" />
+					<meta property="og:image:type" content="image/png" /> */}
+					{/* <meta property="fb:app_id" content={`${FB_APP_ID}`} /> */}
 					<meta name="description" content="Single product page"/>
 				</Helmet>
 				<div className="singleProductPage" style={{backgroundColor: "white", paddingTop: "10px", paddingBottom: "110px"}} >
