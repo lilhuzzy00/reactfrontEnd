412dd0e416a248cce67cb198f53c7c6cc02b9b04
Godspower <chimabest70@gmail.com>
2021-08-27 22:38:45 +0100

    adding helmet

--- src/core/Product.js
+++ src/core/Product.js
@@ -42,19 +42,19 @@ const Products = (props) => {
 
 
 	return(
-
+		<Helmet>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>{product.name}</title>
+				<meta property="og:url" content={`${API}/product/${product._id}`}/>
+				<meta property="og:image" content={`${API}/product/photo/${product._id}`}/>
+				{/* <meta property="og:image:type" content="image/jpg" />
+				<meta property="og:image:type" content="image/png" />
+				<meta property="fb:app_id" content={`${FB_APP_ID}`} /> */}
+				<meta name="description" content="Single product page"/>
+		</Helmet>
 		<Layout>
-				<Helmet>
-					<meta charSet="utf-8" />
-					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-					<title>{product.name}</title>
-					<meta property="og:url" content={`${API}/product/${product._id}`}/>
-					<meta property="og:image" content={`${API}/product/photo/${product._id}`}/>
-					{/* <meta property="og:image:type" content="image/jpg" />
-					<meta property="og:image:type" content="image/png" />
-					<meta property="fb:app_id" content={`${FB_APP_ID}`} /> */}
-					<meta name="description" content="Single product page"/>
-				</Helmet>
+				
 				<div className="singleProductPage" style={{backgroundColor: "white", paddingTop: "10px", paddingBottom: "110px"}} >
 					<div className="imageRow">
 						<ShowPhoto item={product} url="product" />
