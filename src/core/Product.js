4fb1bcc537e08065cdb953566cebf531c9f50dc1
Godspower <chimabest70@gmail.com>
2021-08-27 22:47:09 +0100

    add

--- src/core/Product.js
+++ src/core/Product.js
@@ -52,9 +52,9 @@ const Products = (props) => {
 					<title>{product.name}</title>
 					<meta property="og:url" content={`${API}/product/${product._id}`}/>
 					<meta property="og:image" content={`${API}/product/photo/${product._id}`}/>
-					{/* <meta property="og:image:type" content="image/jpg" />
+					<meta property="og:image:type" content="image/jpg" />
 					<meta property="og:image:type" content="image/png" />
-					<meta property="fb:app_id" content={`${FB_APP_ID}`} /> */}
+					<meta property="fb:app_id" content={`${FB_APP_ID}`} />
 					<meta name="description" content="Single product page"/>
 				</Helmet>
 					<div className="imageRow">
