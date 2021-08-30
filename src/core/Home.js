9e39cdbdc962b1a4236beaba5e857d9211647dcb
Godspower <chimabest70@gmail.com>
2021-08-30 11:46:04 +0100

    adding helmet to all pages

--- src/core/Home.js
+++ src/core/Home.js
@@ -89,7 +89,7 @@ const Home = () =>{
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>Home Page</title>
-				<meta name="description" content="Home page. View our list of products and shop"/>
+				<meta name="description" content="House of Goodiz"/>
 			</Helmet>
 
 {/*				<Searched />
