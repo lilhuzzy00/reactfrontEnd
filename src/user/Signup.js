e4ecd2120c93bd6ada1b5c3fdcbc620ce20a8736
Godspower <chimabest70@gmail.com>
2021-08-30 10:46:38 +0100

    adding

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -2,6 +2,8 @@ import React, {useState} from "react"
 import { Link, Redirect } from "react-router-dom"
 import Layout from "../core/Layout"
 import { signup } from "../auth"
+import {Helmet} from "react-helmet"
+
 
 const Signup = () =>{
 
@@ -90,6 +92,12 @@ const Signup = () =>{
 			description="Signup to node js app"
 			className="container col-md-4 offset-md-4"
 		>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>Register</title>
+				<meta name="description" content="Signup here to continue shopping"/>
+			</Helmet>
 			{showSuccess()}
 			{showError()}
 			{signUpForm()}
